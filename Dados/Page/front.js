f646181e87a679718c011b6bf8e7094751e25e7b
Jonas <web@emd.art.br>
2021-08-05 04:00:11 -0300

    interative front 02

--- Dados/Page/front.js
+++ Dados/Page/front.js
@@ -1,8 +1,8 @@
 let button = document.querySelector('button');
 
 button.onclick = function () {
-     let nome = prompt('Qual é o seu nome?');
-     alert('Olá ' + nome + ', é um prazer te ver!');
+     let profissional = JSON.parse(json)
+  
 }
 
 
