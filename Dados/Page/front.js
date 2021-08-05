5f063b99246a6dfe597c7d4f9023a8a989c3a981
Jonas <web@emd.art.br>
2021-08-04 21:32:59 -0300

    aplicando js

--- Dados/Page/front.js
+++ Dados/Page/front.js
@@ -1,5 +1,30 @@
-/* const mensagem = "Aguarde, estou processando";
-alert(mensagem);*/ 
+//Declarando variavéis globais (id do html)
+let inputSearch = null,
+     buttonSearch = null,
+     panelUser = null,
+     panelStatistics = null;
+
+//Carregando os elementos
+window.addEventListener('load', () => {
+     mapElements();
+     fetchUsers();
+});
+
+//lendo um arquivo externo com json
+async function fetchUsers() {
+     const res = await fetch('dados.json');
+     const json = await res.json();
+     console.log(json);
+}
+
+
+//elementos que irão retornar as informações do arquivo json
+function mapElements() {
+     inputSearch = document.querySelector('#inputSearch');
+     buttonSearch = document.querySelector('#buttonSearch');
+     panelUser = document.querySelector('#panelUser');
+     panelStatistics = document.querySelector('#panelStatistics');
+}
 
 
 
